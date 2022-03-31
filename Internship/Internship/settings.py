b67b85db2f72287ec8eb99bf662d6c6316552f53
Dave-ace <davidoyekanmi.t@gmail.com>
2022-03-31 18:00:20 +0100

    'env'

--- Internship/Internship/settings.py
+++ Internship/Internship/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6%&w_nu%j%7*owlrfzuf$)_ibco92mjj88e!87o0mu1r@4zc-_'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
